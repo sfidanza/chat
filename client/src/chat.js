c9f3900294133bb9d54cc11d0da8d9269694521a
Stephane Fidanza <stephane.fidanza@gmail.com>
2021-08-14 13:43:09 +0200

    Support local video and local screen display

--- client/src/chat.js
+++ client/src/chat.js
@@ -128,7 +128,10 @@ chat.selectRoom = function (room, roomElement) {
             document.querySelector('#room-content').classList.remove('hidden');
             document.querySelector('#user-entry').classList.remove('hidden');
             if (chat.admin) {
-                document.querySelector('#admin-panel').classList.remove('hidden');
+                document.querySelectorAll('[data-admin]').forEach(item => {
+                    item.classList.remove('hidden');
+                });
+                // document.querySelector('#admin-panel').classList.remove('hidden');
             }
         }
         this.activeRoom = room;
@@ -140,6 +143,61 @@ chat.selectRoom = function (room, roomElement) {
             value: this.activeRoom
         });
     }
+};
+
+chat.video = {
+    presets: {
+        video: { 'video': true },
+        all: { 'video': true, 'audio': true },
+        qvga: { 'video': { 'width': { 'exact': 320 }, 'height': { 'exact': 240 } } },
+        vga: { 'video': { 'width': { 'exact': 640 }, 'height': { 'exact': 480 } } },
+        hd: { 'video': { 'width': { 'exact': 1280 }, 'height': { 'exact': 720 } } }
+    },
+    stream: null
+};
+
+chat.video.displayStream = function (stream) {
+    this.stream = stream;
+    document.querySelector('video#localVideoFeed').srcObject = stream;
+    document.querySelector('#device-name').innerHTML = stream.getVideoTracks()[0].label;
+    document.querySelector('#video-overlay').classList.remove('hidden');
+    stream.getVideoTracks()[0].addEventListener('ended', () => {
+        console.log('Stream has ended.');
+    });
+};
+
+chat.video.setConstraint = function (name) {
+    if (this.presets[name]) {
+        this.stream.getVideoTracks()[0].applyConstraints(this.presets[name].video);
+    }
+};
+
+chat.video.openCamera = function () {
+    navigator.mediaDevices.getUserMedia(this.presets.video)
+        .then(this.displayStream.bind(this))
+        .catch(error => {
+            console.error('Error accessing media devices.', error);
+        });
+};
+
+chat.video.openScreen = function () {
+    navigator.mediaDevices.getDisplayMedia({ 'video': true })
+        .then(this.displayStream.bind(this))
+        .catch(error => {
+            console.error('Error accessing media devices.', error);
+        });
+};
+
+chat.video.close = function () {
+    if (this.stream) { // stop using the camera
+        this.stream.getTracks().forEach(track => {
+            track.stop();
+        });
+        this.stream = null;
+    }
+    document.querySelector('video#localVideoFeed').srcObject = null;
+    document.querySelector('#device-name').innerHTML = '';
+    document.querySelector('#video-overlay').classList.add('hidden');
 };
 
 window.addEventListener('load', function () {
